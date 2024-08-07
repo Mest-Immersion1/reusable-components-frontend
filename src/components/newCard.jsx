6129c6d44d662daa32ec64adb12f7c131961fb40
Monica Edem Kokovena <monicaekokovena@.com>
2024-08-07 15:38:52 +0000

    first commit

--- src/components/newCard.jsx
+++ src/components/newCard.jsx
@@ -3,16 +3,21 @@ import { EllipsisVertical } from "lucide-react";
 
 const NewCard = ({ imageSrc, newsOutletName, newsOutletIcon, title, time, link }) => {
   return (
-    <a href={link} className="bg-transparent flex flex-col w-full md:w-[50%] p-2 gap-y-1">
+    <a href={link} className="bg-transparent flex flex-col w-full md:w-[50%] p-2 gap-y-1 ">
       <img src={imageSrc} alt={title} className="rounded-[15px] w-full" />
       
-      <div className="flex justify-between mt-2">
+      <div className="flex justify-between mt-2  relative group">
         <div className="flex items-center gap-[3px]">
           <img src={newsOutletIcon} alt={newsOutletName} className="w-6 h-5"/>
           <span className="text-gray-800 text-[15px]">{newsOutletName}</span>
         </div>
-        
-        <button><EllipsisVertical/></button>
+
+        <div className="flex items-center">
+          <button className="hidden group-hover:flex items-center justify-center w-8 h-8 rounded-full bg-gray-200 hover:bg-gray-300">
+            <EllipsisVertical />
+          </button>
+          <span className="absolute top-12 transform -translate-y-1/2 bg-gray-700 text-sm opacity-0 group-hover:opacity-100 text-white p-1">More</span>
+        </div>
       </div>
       
       <span className="text-[20px] hover:underline text-gray-900 w-full font-medium">{title}</span>
